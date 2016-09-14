5de754cc59f2557f8510526d3f70d792c94714ea
Martin Wiesner <martin4002@gmail.com>
2016-09-14 20:44:15 +0200

    Added tasks list in DisplayActivity

--- app/src/main/java/me/martinwiesner/dailyplanner/DisplayActivity.java
+++ app/src/main/java/me/martinwiesner/dailyplanner/DisplayActivity.java
@@ -3,17 +3,50 @@ package me.martinwiesner.dailyplanner;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DisplayActivity extends AppCompatActivity {
+
+    private String selectedDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+
+        selectedDay = "monday";
+
+        List<Task> tasks = new ArrayList<>();
+        try {
+            FileInputStream fileInputStream = openFileInput(selectedDay + ".dat");
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            tasks = (List<Task>) objectInputStream.readObject();
+            if (tasks == null)
+                tasks = new ArrayList<>();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        updateList(tasks);
     }
 
     @Override
@@ -35,8 +68,46 @@ public class DisplayActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
-        if (view.getId() == R.id.button_edit) {
-            startActivity(new Intent(this, EditActivity.class));
+        String day = "";
+        switch (view.getId()) {
+            case R.id.textview_monday:
+                day = "monday";
+                break;
+            case R.id.textview_tuesday:
+                day = "tuesday";
+                break;
+            case R.id.textview_wednesday:
+                day = "wednesday";
+                break;
+            case R.id.textview_thursday:
+                day = "thursday";
+                break;
+            case R.id.textview_friday:
+                day = "friday";
+                break;
+            case R.id.textview_saturday:
+                day = "saturday";
+                break;
+            case R.id.textview_sunday:
+                day = "sunday";
+                break;
+            default:
+                new Exception("Invalid view").printStackTrace();
         }
+    }
+
+    private void updateList(List<Task> tasks) {
+        Log.e("Display", "length: " + tasks.size());
+        List<String> tasksStrings = new ArrayList<>();
+        for (Task task : tasks) {
+            tasksStrings.add(task.getTask());
+        }
+        if (tasksStrings.size() == 0) {
+            // TODO: 14-Sep-16 Tell the user
+            return;
+        }
+        ArrayAdapter adapter = new ArrayAdapter<>(this, R.layout.listview_edit, tasksStrings);
+        ListView listView = (ListView) findViewById(R.id.listview_edit);
+        listView.setAdapter(adapter);
     }
 }
